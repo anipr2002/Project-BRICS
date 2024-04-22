9b5280d172cd2c27dfa058ca4ceea03cf15a9d1a
youllknowwho <fsociety@pop-os.localdomain>
2024-04-22 12:25:20 +0200

    Added multiple image capture

--- src/capture_images/cam_script.py
+++ src/capture_images/cam_script.py
@@ -3,12 +3,14 @@ import cv2
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 3)
 
+num = 0
 while True:
     ret, frame = cap.read()
     cv2.imshow('frame', frame)
     
     if cv2.waitKey(1) & 0xFF == ord(' '):
-        cv2.imwrite('image.jpg', frame)
+        num += 1
+        cv2.imwrite(f'image_{num}.jpg', frame)
         
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
