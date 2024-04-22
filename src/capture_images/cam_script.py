2266a9ad1e70e83fe68eb5b589ce934be2db0675
CTO <chiragreddy272@gmail.com>
2024-04-22 14:39:33 +0200

    Added function to auto/manual capture

--- src/capture_images/cam_script.py
+++ src/capture_images/cam_script.py
@@ -1,16 +1,23 @@
 import cv2
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(4)
+cap2 = cv2.VideoCapture(2)
 cap.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.3)
 
-num = 0
-while True:
-    ret, frame = cap.read()
-    cv2.imshow('frame', frame)
+def capture(mode = 1, seconds = 10):
+    if mode == 1: #Manual capturing
+        num = 0
+        while True:
+            ret, frame = cap.read()
+            cv2.imshow('frame', frame)
+            
+            if cv2.waitKey(1) & 0xFF == ord(' '):
+                num += 1
+                cv2.imwrite(f'image_{num}.jpg', frame)
+                
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
     
-    if cv2.waitKey(1) & 0xFF == ord(' '):
-        num += 1
-        cv2.imwrite(f'image_{num}.jpg', frame)
-        
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    elif mode == 2:
+        #Auto capturing for seconds
+        pass
