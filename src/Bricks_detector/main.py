4a43c32703ac414d4333d1582135c5666ee898a9
THWS(Server) <chirag.reddy@study.thws.de>
2024-05-06 17:29:27 +0200

    Merged 2 datasets and trained yolov8n

--- src/Bricks_detector/main.py
+++ src/Bricks_detector/main.py
@@ -7,4 +7,7 @@ model = YOLO("yolov8n.yaml")  # build a new model from scratch
 #model = YOLO('yolov8n.pt')  # load a pretrained model (recommended for training)
 #model = YOLO('yolov8n.yaml').load('yolov8n.pt')  # build from YAML and transfer weights
 # Use the model
-model.train(data="antonio/Project-BRICS/src/Bricks_detector/config.yaml", epochs=150)  # train the model
+#model.train(data="antonio/Project-BRICS/src/Bricks_detector/config.yaml", epochs=150)  # train the model
+
+
+model.train(data="/home/reddy/BRICS/data/as_data/data3/data.yaml", epochs=150)  # train the model with Roboflow dataset
