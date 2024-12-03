d47c0a19ec758147220448d5496572031d6cbea1
THWS(Server) <chirag.reddy@study.thws.de>
2024-12-03 17:40:33 +0100

    Added new models

--- src/yolo/train.py
+++ src/yolo/train.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n-obb.yaml")  # build a new model from YAML
-model = YOLO("yolov8n-obb.pt")  # load a pretrained model (recommended for training)
-model = YOLO("yolov8n-obb.yaml").load("yolov8n.pt")  # build from YAML and transfer weights
+# model = YOLO("yolo11n-obb.yaml")  # build a new model from YAML
+model = YOLO("yolo11n-obb.pt")  # load a pretrained model (recommended for training)
+# model = YOLO("yolo11n-obb.yaml").load("yolo11n.pt")  # build from YAML and transfer weights
 
 # Train the model
-results = model.train(data="/data/reddy/first_pass/data.yaml", epochs=100, imgsz=640)
+results = model.train(data="/data/reddy/dataset_blender/data.yaml", epochs=100, imgsz=640, project="/home/reddy/BRICS/chirag/Project-BRICS/models", name=f"001")
